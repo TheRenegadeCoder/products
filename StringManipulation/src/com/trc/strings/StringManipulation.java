69cd0f0a7234dd22b71e9c74c5b5188a81c4baeb
Jeremy Griffith <jrg170@case.edu>
2017-06-17 15:20:40 -0400

    Adding comments already

--- StringManipulation/src/com/trc/strings/StringManipulation.java
+++ StringManipulation/src/com/trc/strings/StringManipulation.java
@@ -3,10 +3,21 @@ package com.trc.strings;
 /**
  * A string manipulation class.
  * 
+ * This class serves as a potential solution for the String Manipulation
+ * problem.
+ * 
  * Copyright (C) 2017 Jeremy Griffith
  */
 public class StringManipulation {
 
+    /**
+     * The isAlphabetical method is used to determine if the characters in a
+     * string are in alphabetical order. Characters are case insensitive and
+     * symbols are ignored.
+     * 
+     * @param input a string under test
+     * @return true if the string is in alphabetical order
+     */
     public static boolean isAlphabetical(String input) {
         char prev = 0;
         for (int i = 0; i < input.length(); i++) {
