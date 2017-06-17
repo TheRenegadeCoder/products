309cd6e1a236fa8b4e0737c0998c06f47875d43e
Jeremy Griffith <jrg170@case.edu>
2017-06-17 15:39:46 -0400

    Adding test cases

--- StringManipulation/test/com/trc/strings/StringManipulationTest.java
+++ StringManipulation/test/com/trc/strings/StringManipulationTest.java
@@ -1,5 +1,6 @@
 package com.trc.strings;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -9,6 +10,8 @@ public class StringManipulationTest {
     @Test
     public void testIsAlphabetical() {
         assertTrue(StringManipulation.isAlphabetical("abcd"));
+        assertTrue(StringManipulation.isAlphabetical("ACdf"));
+        assertFalse(StringManipulation.isAlphabetical("Fed"));
     }
 
 }
