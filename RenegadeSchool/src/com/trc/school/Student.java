7b1599ebfb1a7aed193626c56557a4a36967e182
Jeremy Griffith <jrg170@case.edu>
2017-09-30 23:42:54 -0400

    Created a student class

---
+++ RenegadeSchool/src/com/trc/school/Student.java
@@ -0,0 +1,16 @@
+package com.trc.school;
+
+public class Student {
+    
+    private String firstName;
+    private String lastName;
+    private String address;
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+}
